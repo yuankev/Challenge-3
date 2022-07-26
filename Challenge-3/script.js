bc875846e0c53863acc9a432dfb5a2a35f34469b
yuankev <108314012+yuankev@users.noreply.github.com>
2022-07-26 10:40:20 -0700

    Add files via upload

---
+++ Challenge-3/script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = "password";
+
+}
+
+// Add event listener to generate button
+var generateBtn.addEventListener ("click",writePassword)
+  console,log = passwordText;
+
+function.addEventListener ( "click", generateBtn); {
+  if (passwordText = password) {
+    console.log ="password"
+  }
+}
